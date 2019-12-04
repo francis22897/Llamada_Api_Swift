89b3199fd3f088b64dd11a77a3f8bf2fcdc694ed
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2019-12-04 18:37:50 +0100

    Initial Commit

---
+++ Api_RickMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Api_RickMorty
+//
+//  Created by francisco.adan on 04/12/2019.
+//  Copyright © 2019 francisco.adan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
